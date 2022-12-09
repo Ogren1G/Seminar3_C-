75db51d22e9df374058da47ff879e9502c918bbe
Gevorg <gevacreature2016@mail.ru>
2022-12-09 20:13:05 +0300

    zadanie19

---
+++ Zadanie19/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите пятизначное число");
+
+int n = int.Parse(Console.ReadLine());
+int a = n / 10000;
+int b = n % 10;
+int c = (n % 10000) / 1000;
+int d = (n % 100) / 10;
+
+if(a == b && c == d)
+{
+    Console.WriteLine("Это полиндром!!!");
+}
+else
+{
+    Console.WriteLine("Это не полиндром :(");
+}
